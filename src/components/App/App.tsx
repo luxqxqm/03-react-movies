4465d4ebe0f22772c60339727bb29d5d5389b487
Den Yaseniuk <denasenuk@gmail.com>
2026-04-08 12:02:38 +0200

    hw3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import fetchMovies from "./services/movieService";
-import SearchBar from "./components/SearchBar/SearchBar";
+import fetchMovies from "../../services/movieService";
+import SearchBar from "../SearchBar/SearchBar";
 import { Toaster, toast } from "react-hot-toast";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import type { Movie } from "./types/movie";
-import MovieModal from "./components/MovieModal/MovieModal";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import type { Movie } from "../../types/movie";
+import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./components/App/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <>
